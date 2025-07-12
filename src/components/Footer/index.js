9ce0b047b85c63b33704dc5c92a6aab8076c2bc7
Samarth Jaiswal <samjaiswal51@gmail.com>
2025-07-13 00:15:19 +0530

    some changes

---
+++ src/components/Footer/index.js
@@ -0,0 +1,6 @@
+import Footer from './Footer';
+import FooterLinks from './FooterLinks';
+import SocialIcons from './SocialIcons';
+import Newsletter from './Newsletter';
+
+export { Footer, FooterLinks, SocialIcons, Newsletter };
